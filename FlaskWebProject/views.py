4ef576f003fcd0e8f65fd3262353f0707f332cff
Pranshu Vaish <pranshu.vaish@chevron.com>
2026-08-27 12:54:46 +0530

    code fix: Updated requirements.txt to reflect changes in dependencies and Python version. Managing Requests. Correction to files. Changing the Blob service to use BlobServiceClient and updating Python version to 3.12. Correction to files. Changing the Blob service to use BlobServiceClient and updating Python version to 3.12. Changing Python version.

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -90,7 +90,11 @@ def authorized():
         result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
             request.args['code'],
             scopes=Config.SCOPE,
-            redirect_uri=url_for('authorized', _external=True),
+            redirect_uri=url_for(
+                'authorized',
+                _external=True,
+                _scheme='https',
+            ),
         )
         if "error" in result:
             return render_template("auth_error.html", result=result)
@@ -144,5 +148,9 @@ def _build_auth_url(authority=None, scopes=None, state=None):
     return _build_msal_app(authority=authority).get_authorization_request_url(
         scopes or Config.SCOPE,
         state=state,
-        redirect_uri=url_for('authorized', _external=True),
+        redirect_uri=url_for(
+            'authorized',
+            _external=True,
+            _scheme='https',
+        ),
     )
